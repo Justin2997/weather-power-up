d66fef9281cc9d0d2e48e3d45841bb1e1d9de470
Justin2997 <justin.brlotte797@gmail.com>
2019-07-02 09:43:52 -0400

    Adding DoneCard

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,22 @@
 console.log('Simple Kamban it alive');
 
+var numberOfElementTotal = 0;
+var numberOfElementToday = 0;
+var DoneCard = [];
+
 window.TrelloPowerUp.initialize({
     'card-badges': function(t, opts) {
+
       // return an array of card badges
       return t.card('name', 'dateLastActivity', 'labels', 'idList')
-      .then(function(card) {
-        console.log(card);
-        return [{
-          text: card.isShort
-        }];
-      })
+        .then(function(card) {
+          if (card.idList == "5b09bf74146fca0cbc15f1ab") {        // Id for Terminer list
+            DoneCard.push(card);
+          }
+          return DoneCard;
+        })
+        .then(function() {
+          console.log(DoneCard);
+        });
   }
 });
