a4477f92c405ddbd27ab5b94f62b08ba42e16493
Justin2997 <justin.brlotte797@gmail.com>
2019-07-02 11:13:43 -0400

    Adding Promise All

--- src/js/connector.js
+++ src/js/connector.js
@@ -10,13 +10,13 @@ window.TrelloPowerUp.initialize({
     'card-badges': function(t, opts) {
 
       // return an array of card badges
-      return t.card('name', 'dateLastActivity', 'labels', 'idList')
-        .then(function(card) {
-          if (card.idList == "5b09bf74146fca0cbc15f1ab") {        // Id for Terminer list
-            DoneCard.push(card);
-          }
-          return DoneCard;
-        })
+      return Promise.all(t.card('name', 'dateLastActivity', 'labels', 'idList').then(function(card) {
+            if (card.idList == "5b09bf74146fca0cbc15f1ab") {        // Id for Terminer list
+              DoneCard.push(card);
+            }
+            return DoneCard;
+          })
+        )
         .finally(function() {
           for (var card in DoneCard){
             var labels = card.labels
