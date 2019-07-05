9d4451e50eb1bbd4b952ad4698878ec7ea908247
Justin2997 <justin.brlotte797@gmail.com>
2019-07-05 08:48:50 -0400

    Change Console log

--- src/js/connector.js
+++ src/js/connector.js
@@ -7,39 +7,37 @@ var LabelsList = [];
 var NumberOfLabels = 0;
 
 window.TrelloPowerUp.initialize({
-    'card-badges': function(t, opts) {
+  'card-badges': function (t, opts) {
 
-      // return an array of card badges
-      return Promise.all(t.card('name', 'dateLastActivity', 'labels', 'idList').then(function(card) {
-            if (card.idList == "5b09bf74146fca0cbc15f1ab") {        // Id for Terminer list
-              DoneCard.push(card);
-            }
-            return DoneCard;
-          })
-        )
-        .finally(function() {
-          for (var card in DoneCard){
-            var labels = card.labels
+    // return an array of card badges
+    return Promise.all(t.card('name', 'dateLastActivity', 'labels', 'idList').then(function (card) {
+      if (card.idList == "5b09bf74146fca0cbc15f1ab") {        // Id for Terminer list
+        DoneCard.push(card);
+      }
+      return DoneCard;
+    })
+    )
+      .finally(function () {
+        for (var card in DoneCard) {
+          var labels = card.labels
 
-            // Find the list of Label
-            for (var label in labels) {
-              NumberOfLabels++;
-              var toAdd = true;
-              for (var string in LabelsList) {
-                if (string == label) {
-                  toAdd = false;
-                }
-              }
-              if (toAdd) {
-                LabelsList.push(label);
+          // Find the list of Label
+          for (var label in labels) {
+            NumberOfLabels++;
+            var toAdd = true;
+            for (var string in LabelsList) {
+              if (string == label) {
+                toAdd = false;
               }
             }
-
+            if (toAdd) {
+              LabelsList.push(label);
+            }
           }
 
-          console.log(DoneCard);
-          console.log(LabelsList);
-          console.log(NumberOfLabels);
-        })
-    }
+        }
+
+        console.log(DoneCard);
+      })
+  }
 });
